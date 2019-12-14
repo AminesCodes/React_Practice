cd2d3901a764938d6c409f2c9ce2d51fec302987
AminesCodes <aminebensalem@pursuit.org>
2019-12-14 17:46:29 -0500

    v2 with  network request for Dogs API

--- practice/src/App.js
+++ practice/src/App.js
@@ -3,35 +3,86 @@ import React from 'react';
 import './App.css';
 
 import Dog from './Components/Dog';
+import BreedList from './Components/BreedList'
 
 
 class App extends React.PureComponent {
   state = {
-    imageURL: ''
+    imageURLs: [],
+    breeds: null,
+    subBreeds: [],
+    targetBreed: '',
+    targetSubBreed: ''
   }
 
-  handleClickNewDog = () => {
-    this.getDogPicture()
-  }
-
-  getDogPicture = async () => {
-    const baseURL = 'https://dog.ceo/api/breeds/image/random'
-
+  async componentDidMount() {
     try {
-      // const {data} = await axios.get(baseURL)
-      // this.setState({imageURL: data.message})
-      const {data: {message}} = await axios.get(baseURL)
-      this.setState({imageURL: message})
+      const {data} = await axios.get('https://dog.ceo/api/breeds/list/all')
+      this.setState({breeds: data.message})
+      this.getDogPicture(null, null)
     } catch (err) {
       console.log(err)
     }
   }
 
+  handleClickNewDogButton = (breed, subBreed) => {
+    this.getDogPicture(breed, subBreed)
+  }
+
+  getDogPicture = async (breed, subBreed) => {
+    let baseURL = 'https://dog.ceo/api/breeds/image/random/10'
+
+    if (breed && subBreed) {
+      baseURL = `https://dog.ceo/api/breed/${breed}/${subBreed}/images/random/10`
+    } else if (breed) {
+      baseURL = `https://dog.ceo/api/breed/${breed}/images/random/10`
+    }
+    
+    try {
+      // const {data} = await axios.get(baseURL)
+      // this.setState({imageURLs: data.message})
+      const {data: {message}} = await axios.get(baseURL)
+      this.setState({imageURLs: message})
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  handleSelectChange = breed => {
+    this.setState((prevState) => ({
+      targetBreed: breed, 
+      subBreeds: prevState.breeds[breed],
+      targetSubBreed: ''
+    }))
+
+    this.getDogPicture(breed, null)
+  }
+
+  handleSubBreedSelectChange = subBreed => {
+    this.setState({targetSubBreed: subBreed})
+
+    this.getDogPicture(this.state.targetBreed, subBreed)
+  }
+
   //########################### RENDER #########################
   render() {
+    const allImages = this.state.imageURLs.map(url => <Dog key={url} imageURL={url}/>)
+    
     return (
       <div className="App">
-        <Dog handleClick={this.handleClickNewDog} imageURL={this.state.imageURL} />
+        <BreedList 
+            breeds={this.state.breeds} 
+            breed={this.state.targetBreed}
+            subBreeds={this.state.subBreeds}
+            subBreed={this.state.targetSubBreed}
+            handleChange={this.handleSelectChange} 
+            handleSubBreedChange={this.handleSubBreedSelectChange}
+            updateImage={this.getDogPicture}
+            handleClick={this.handleClickNewDogButton} 
+        />
+        <br />
+
+        {allImages}
       </div>
     );
   }
--- practice/src/Components/Dog.jsx
+++ practice/src/Components/Dog.jsx
@@ -1,22 +1,16 @@
 import React from 'react';
 
 export default class Dog extends React.Component {
-    componentDidMount() {
-        this.props.handleClick()
-    }
+
+    
 
     render() {
         return (
-            <>
-                <h1>Random Dog Pic Generator</h1>
-                <img 
-                    alt='Random Dog' 
-                    src={this.props.imageURL}>
-                    {/* style={{img: {border: '2px black solid'}}} */}
-                </img>
-                <br />
-                <button onClick={this.props.handleClick}>New Dog</button>
-            </>
+            <img 
+                alt='Random Dog' 
+                src={this.props.imageURL}
+                style={{margin: '5px', height: '250px', border: '2px black solid'}}>
+            </img>
         )
     }
 }
